c6d4f72b81986144eb6396c3b715bc643a643501
minhdoanhk4 <doanhminh237@gmail.com>
2025-11-11 22:35:38 +0700

    chuc nang loaded data grid

---
+++ ManagementMusicPlayer.MinhDA/ManagementMusicPlayer.BLL/Services/CompanyService.cs
@@ -0,0 +1,20 @@
+﻿using ManagementMusicPlayer.DAL.Entities;
+using ManagementMusicPlayer.DAL.Reposistories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementMusicPlayer.BLL.Services
+{
+    public class CompanyService
+    {
+        private CompanyRepo _repo = new();
+
+        public List<Company> GetAllSupplier()
+        {
+            return _repo.GetAll();
+        }
+    }
+}
---
+++ ManagementMusicPlayer.MinhDA/ManagementMusicPlayer.DAL/Reposistories/MusicPlayerRepo.cs
@@ -0,0 +1,21 @@
+﻿using ManagementMusicPlayer.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementMusicPlayer.DAL.Reposistories
+{
+    public class MusicPlayerRepo
+    {
+        private ManagementMusicPlayerContext _ctx;
+
+        public List<MusicPlayer> GetAll()
+        {
+            _ctx = new();
+            return _ctx.MusicPlayers.Include("Company").ToList();
+        }
+    }
+}
