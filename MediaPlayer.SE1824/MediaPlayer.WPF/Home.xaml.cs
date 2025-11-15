3035c1834bdeb4d3a822dd41694c040957cd17e3
Trương Minh Nhật <fleeforezz@gmail.com>
2025-11-15 09:17:48 +0700

    Update

--- MediaPlayer.SE1824/MediaPlayer.WPF/Home.xaml.cs
+++ MediaPlayer.SE1824/MediaPlayer.WPF/Home.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MediaPlayer.BLL.Services;
+using MediaPlayer.DAL.Entities;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MediaPlayer.WPF
 {
@@ -19,14 +23,43 @@ namespace MediaPlayer.WPF
     /// </summary>
     public partial class Home : Window
     {
+        private SongService _songService = new();
+        private ObservableCollection<Song> songs = new ObservableCollection<Song>();
+        private Song currentSong;
+        private DispatcherTimer timer;
+
         public Home()
         {
             InitializeComponent();
+
+            // Load songs
+            LoadSongs();
+            lvSongs.ItemsSource = songs;
+        }
+
+        private void LoadSongs()
+        {
+            songs.Clear();
+
+            try
+            {
+                var songFromDb = _songService.GetAllSongs();
+                foreach (var song in songFromDb)
+                {
+                    songs.Add(song);
+                }
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show($"Error loading songs: {ex.Message}", "Database Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void BtnUploadMusic_Click(object sender, RoutedEventArgs e)
         {
-
+            UploadMusicDialog uploadMusicDialog = new UploadMusicDialog();
+            uploadMusicDialog.ShowDialog();
         }
 
         private void BtnPlay_Click(object sender, RoutedEventArgs e)
